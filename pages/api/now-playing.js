963e01b46eada4b25008b1a079219a86382a594d
Nasser Setti <Abdennasser.Essati@4d.com>
2020-10-12 09:05:08 +0100

    chore: add spotify to the portfolio

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -2,8 +2,7 @@ import { getNowPlaying } from '../../lib/spotify';
 
 export default async (_, res) => {
   const response = await getNowPlaying();
-  console.log(response);
-  if (response.status === 204 || response.status > 400) {
+  if (response.status === 204 || response.status >= 400) {
     return res.status(200).json({ isPlaying: false });
   }
 
@@ -14,7 +13,6 @@ export default async (_, res) => {
   const album = song.item.album.name;
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
-
   return res.status(200).json({
     album,
     albumImageUrl,
